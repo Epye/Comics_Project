df121c9ceb86036eb1857fe28220c1bf7f55f7d0
Nicolas <nicolas.quillon@etu.univ-lyon1.fr>
2017-12-08 09:37:54 +0100

    [FEAT] Init Comics

---
+++ app/src/main/java/nicolas/quillon/iem/gestion_comics/model/pojo/Comics.java
@@ -0,0 +1,11 @@
+package nicolas.quillon.iem.gestion_comics.model.pojo;
+
+import java.util.ArrayList;
+
+/**
+ * Created by iem on 08/12/2017.
+ */
+
+public class Comics extends ArrayList<Comic>{
+
+}
---
+++ app/src/main/java/nicolas/quillon/iem/gestion_comics/model/pojo/Creator.java
@@ -0,0 +1,15 @@
+package nicolas.quillon.iem.gestion_comics.model.pojo;
+
+/**
+ * Created by iem on 08/12/2017.
+ */
+
+public class Creator {
+    private String name;
+    private String role;
+
+    public Creator(String name, String role) {
+        this.name = name;
+        this.role = role;
+    }
+}
