8dbee8f22d735af1f879127cd0173feeb47ccddd
Nicolas <nicolas.quillon@etu.univ-lyon1.fr>
2017-12-08 11:10:53 +0100

    [FIX] Travis build + Try Artifact

--- build-apk.sh
+++ build-apk.sh
@@ -2,14 +2,14 @@
 set -x
 
 #REQUIRED
-apk_origin_path=app/build/outputs/apk/app-debug.apk
+apk_origin_path=app/build/outputs/apk/debug/app-debug.apk
 apk_name=sample
 apk_extension=.apk
 apk_folder=build/apk/
 
 #LET'S SCRIPT
 #GET FINAL APK_PATH
-date=$(date '+%d%m%Y%H%M%S');
+date=$(date '+%d%m%Y%H%M');
 apk_final_path=$apk_folder$apk_name-$date$apk_extension
 
 #BUILD APK
