c88c60cd30662112090acd16f6f29791ac2ee047
Johan <johan.lannier@etu.univ-lyon1.fr>
2017-12-12 12:18:40 +0100

    [FIX] Start asking permissions

---
+++ app/src/main/java/nicolas/quillon/iem/gestion_comics/Modele/manager/PermissionsManager.java
@@ -0,0 +1,20 @@
+package nicolas.quillon.iem.gestion_comics.Modele.manager;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by iem on 12/12/2017.
+ */
+
+public class PermissionsManager {
+
+    public PermissionsManager(Context context, String[] permissionsList) {
+
+    }
+
+
+}
--- app/src/main/java/nicolas/quillon/iem/gestion_comics/ui/Vue/MainActivity.java
+++ app/src/main/java/nicolas/quillon/iem/gestion_comics/ui/Vue/MainActivity.java
@@ -1,10 +1,15 @@
 package nicolas.quillon.iem.gestion_comics.ui.Vue;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import nicolas.quillon.iem.gestion_comics.Modele.manager.JSONManager;
 import nicolas.quillon.iem.gestion_comics.Modele.pojo.Comics;
@@ -28,9 +33,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        askForPermission();
+
+        //------------------------------------------------------------------>>>>>>>>>>>>>>> Desactiver JSON si permission refusée
         //Initialisation du fichier JSON
         jsonManager=new JSONManager("/data/sample-ok.json"); //dossier data à la racine du tel (pas sur SD)
-        jsonManager.init();
 
         //ad - appel de la fonction d'initialisation
         InitializeView();
@@ -52,6 +59,36 @@ public class MainActivity extends AppCompatActivity {
 
         listViewComic.setAdapter(adapterComics);
 
+    }
+
+    private void askForPermission(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                Toast.makeText(this, "L'accès aux données est nécessaire pour visualiser les comics", Toast.LENGTH_SHORT).show();
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        1);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[],int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // La permission est garantie
+                } else {
+                    Toast.makeText(this, "Les comics ne pourront pas être chargés", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
     }
 
     //endregion
